fda4003beed9a40d36a1e8110d75f4728a1b3bba
ChigaBiga <bohdan_bochulya@ukr.net>
2020-01-24 01:44:50 +0200

    data base connector and related dependensies added

--- src/main/java/com/bohdan/MobileWebWsApplication.java
+++ src/main/java/com/bohdan/MobileWebWsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MobileWebWsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MobileWebWsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MobileWebWsApplication.class, args);
+    }
 
 }

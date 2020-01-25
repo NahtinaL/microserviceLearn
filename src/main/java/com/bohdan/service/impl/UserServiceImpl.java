6111449db26a8205f4ccef1fb913efe97accb353
ChigaBiga <bohdan_bochulya@ukr.net>
2020-01-26 01:52:02 +0200

    sring security added + public user id generation implemented

--- src/main/java/com/bohdan/service/impl/UserServiceImpl.java
+++ src/main/java/com/bohdan/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.bohdan.service.impl;
 import com.bohdan.UserRepository;
 import com.bohdan.io.entity.UserEntity;
 import com.bohdan.service.UserService;
+import com.bohdan.shared.Utils;
 import com.bohdan.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,14 +15,24 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    Utils utils;
+
     @Override
     public UserDto createUser(UserDto user) {
+
+        UserEntity storedUserDetails = userRepository.findByEmail(user.getEmail());
+        if  (storedUserDetails != null) {
+            throw new RuntimeException("Record already exist");
+        }
+
         UserEntity userEntity = new UserEntity();
         BeanUtils.copyProperties(user, userEntity);
         userEntity.setEncryptedPassword("test");
-        userEntity.setUserId("testUserId");
+        String publicUserId = utils.generateUseerId(30);
+        userEntity.setUserId(publicUserId);
 
-        UserEntity storedUserDetails = userRepository.save(userEntity);
+        storedUserDetails = userRepository.save(userEntity);
 
         UserDto returnValue = new UserDto();
         BeanUtils.copyProperties(storedUserDetails, returnValue);
---
+++ src/main/java/com/bohdan/shared/Utils.java
@@ -0,0 +1,27 @@
+package com.bohdan.shared;
+
+import org.springframework.stereotype.Component;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+@Component
+public class Utils {
+
+    private final Random RANDOM = new SecureRandom();
+    private final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private final int ITERATIONS = 1000;
+    private final int KEY_LENGH = 256;
+
+    public String generateUseerId(int  length) {
+        return generateRandomString(length);
+    }
+
+    private String generateRandomString(int length) {
+        StringBuilder returnValue = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            returnValue.append(ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length())));
+        }
+        return new String(returnValue);
+    }
+}
